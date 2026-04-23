91a1046acd75bf4836186f2c9c3cd4b4034682d5
Ridham44 <ridhampatel2004.44@gmail.com>
2026-04-23 15:08:26 +0530

    minor fix

--- backend/middleware/authorizeAdmin.middleware.js
+++ backend/middleware/authorizeAdmin.middleware.js
@@ -3,7 +3,7 @@ const status = require('../utils/statusCodes');
 module.exports = (req, res, next) => {
     try {
         if (!req.admin || req.admin.role !== 'admin') {
-            console.log('Unauthorized access attempt by user:', req.admin ? req.admin.id : 'Unknown');
+           // console.log('Unauthorized access attempt by user:', req.admin ? req.admin.id : 'Unknown');
             return res.status(status.Forbidden).json({
                 success: false,
                 message: 'Access denied. Admin only',
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const controller = require('../controllers/product.controller');
 const validation = require('../validations/product.validation');
-const adminAuth = require('../middleware/authorizeAdmin.middleware');
+const adminOnly = require('../middleware/authorizeAdmin.middleware');
+const adminAuth = require('../middleware/adminAuth.middleware');
 
-router.post('/', adminAuth, validation.validateCreateProduct, controller.createProduct);
+router.post('/', adminAuth, adminOnly, validation.validateCreateProduct, controller.createProduct);
 router.get('/', controller.getProductList);
 router.get('/:id', controller.getProductById);
-router.put('/:id', adminAuth, validation.validateUpdateProduct, controller.updateProduct);
-router.delete('/:id', adminAuth, controller.deleteProduct);
+router.put('/:id', adminAuth, adminOnly, validation.validateUpdateProduct, controller.updateProduct);
+router.delete('/:id', adminAuth, adminOnly, controller.deleteProduct);
 
 module.exports = router;
